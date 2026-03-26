c57de15525c6e624d105c348787398f1d5790bf1
hbotond97 <hbotond97@gmail.com>
2026-03-26 12:39:00 +0000

    updated name and removed app bar

--- lib/main.dart
+++ lib/main.dart
@@ -56,10 +56,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       backgroundColor: _isBolc() ? Colors.red : Colors.green,
       body: GestureDetector(
         onTap: _incrementCounter,
